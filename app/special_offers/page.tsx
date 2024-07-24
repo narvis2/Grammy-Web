661319552f3b855d32a44d4ef32d31e9087039e6
narvis2 <narvis2@douzone.com>
2024-07-24 10:13:37 +0900

    Offers -> 시설 및 서비스 작업 완료

--- app/special_offers/page.tsx
+++ app/special_offers/page.tsx
@@ -2,12 +2,13 @@
 
 import React, { useState, useEffect, useMemo } from "react";
 import Image from "next/image";
-import { OfferModel } from "@/data/model/offer/types";
+import { OfferModel, OfferServiceModel } from "@/data/model/offer/types";
 import { OFFER_TYPE } from "@/data/model/offer/enum";
 import OfferIcon from "@/components/offers/OfferIcon";
 import { useBedTypeList } from "@/data/hooks";
 import { BedModel } from "@/data/model/bed/types";
 import OfferBedAdapter from "@/components/offers/adapter/OfferBedAdapter";
+import OfferServiceAdapter from "@/components/offers/adapter/OfferServiceAdapter";
 
 const images = [
   { src: "/images/room1.jpg", width: 600, height: 600 },
@@ -37,10 +38,24 @@ const offers: OfferModel[] = [
     description: "카페를 즐기실 수 있습니다.",
   },
   {
-    type: OFFER_TYPE.TOURIST,
+    type: OFFER_TYPE.SERVICE,
     image: "/images/room2.jpg",
-    description: "관광지 주변에 위치하고 있습니다.",
+    description: "그라미 호텔에서는 아래와 같은 시설 및 서비스를 제공합니다.",
   },
+];
+
+const serviceList: OfferServiceModel[] = [
+  { title: "주차가능" },
+  { title: "야외테라스 객실" },
+  { title: "스타일러" },
+  { title: "객실금연" },
+  { title: "객실금연" },
+  { title: "오션뷰 객실" },
+  { title: "마운틴뷰 객실" },
+  { title: "구글 스마트 TV" },
+  { title: "OTT(넷플릭스)" },
+  { title: "와이파이" },
+  { title: "에어컨" },
 ];
 
 const SpecialOffers = () => {
@@ -170,33 +185,25 @@ const SpecialOffers = () => {
           <h5 className="tit">GRAMMY HOTEL</h5>
         </div>
 
-        <OfferIcon selectedOffer={selectedOffer} />
+        {!!offersInfo && <OfferIcon offers={offersInfo} />}
 
         {!!offersInfo &&
           (selectedOffer === OFFER_TYPE.BED ? (
-            <OfferBedAdapter
-              showText={showText}
-              bedModelList={bedModelList}
-              offers={offersInfo}
-            />
+            <OfferBedAdapter showText={showText} bedModelList={bedModelList} />
+          ) : selectedOffer === OFFER_TYPE.SERVICE ? (
+            <OfferServiceAdapter serviceList={serviceList} />
           ) : (
-            <>
-              <p className="text-center text-xl mt-4">
-                {offersInfo.description}
-              </p>
-
-              <div className="flex justify-center mt-8 mb-12">
-                <Image
-                  src={offersInfo.image}
-                  alt={offersInfo.type}
-                  width={400}
-                  height={400}
-                  className={`object-cover h-96 ${
-                    showText ? "fade-in" : "fade-out"
-                  }`}
-                />
-              </div>
-            </>
+            <div className="flex justify-center mt-8 mb-12">
+              <Image
+                src={offersInfo.image}
+                alt={offersInfo.type}
+                width={400}
+                height={400}
+                className={`object-cover h-96 ${
+                  showText ? "fade-in" : "fade-out"
+                }`}
+              />
+            </div>
           ))}
       </section>
     </div>
