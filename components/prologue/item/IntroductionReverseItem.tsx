89624b0ac2790d9d6fbbe6ce0e31755de7b388ba
narvis2 <choeyeongjun@choeyeongjun-ui-MacBookPro.local>
2025-03-12 13:54:35 +0900

    프롤로그 완성

--- components/prologue/item/IntroductionReverseItem.tsx
+++ components/prologue/item/IntroductionReverseItem.tsx
@@ -7,21 +7,11 @@ type IntroductionItemProps = {
   description: string;
 }
 
-const IntroductionReverseItem = ({img, title, description}: IntroductionItemProps) => {
-  
+const IntroductionReverseItem = ({ img, title, description }: IntroductionItemProps) => {
   return (
     <div className="flex flex-col md:flex-row md:justify-between items-start">
-      {/* 설명 영역 (왼쪽) */}
-      <div className="w-full md:w-1/2">
-        <h2 className="text-xl font-bold mb-2">{title}</h2>
-        <p className="text-gray-700 leading-relaxed" dangerouslySetInnerHTML={{ __html: description }} />
-      </div>
-
-      {/* md 이상에서만 보이는 구분선 (이미지 높이와 동일하게 h-[400px]) */}
-      <div className="hidden md:block h-[400px] border-l border-gray-300 mx-8"></div>
-  
-      {/* 이미지 영역 (오른쪽) */}
-      <div className="w-full md:w-1/2">
+      {/* 이미지 영역: 작은 화면에서는 위에, md 이상에서는 오른쪽에 위치 */}
+      <div className="w-full md:w-1/2 order-1 md:order-3">
         <Image
           src={img}
           alt="Grami"
@@ -30,7 +20,18 @@ const IntroductionReverseItem = ({img, title, description}: IntroductionItemProp
           className="rounded-md shadow"
         />
       </div>
-  </div>
+
+      {/* md 이상에서만 보이는 구분선: 설명과 이미지 사이에 위치 */}
+      {/* <div className="hidden md:block md:order-2 h-[400px] border-l border-gray-300 mx-8"></div> */}
+  
+      {/* 설명 영역: 작은 화면에서는 아래에, md 이상에서는 왼쪽에 위치 */}
+      <div className="w-full md:w-1/2 order-2 md:order-1 mt-4 md:mt-0">
+        <h2 className="text-xl font-bold mb-2">{title}</h2>
+        <p className="text-gray-700 leading-relaxed" dangerouslySetInnerHTML={{ __html: description }} />
+      </div>
+
+      <div className="order-3 md:order-2 w-full md:w-auto h-0.5 md:h-[400px] border-t md:border-t-0 md:border-l border-gray-300 md:mx-8 mt-4 md:mt-0"></div>
+    </div>
   );
 }
 
