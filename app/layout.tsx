16f2e10cac365f9e9a7111448a912e2bf883a294
narvis2 <choeyeongjun@choeyeongjun-ui-MacBookPro.local>
2025-03-13 10:19:47 +0900

    SEO 설정
    - 메타데이터 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,47 @@ import { NextLayout, NextProviders } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "그라미 호텔",
-  description: "포항 그라미 호텔",
+  title: "그라미호텔",
+  description: "포항, 포항호텔, 포항숙소, 그라미호텔, 포항 남구호텔, 풀빌라, 테라스, 오션뷰, 마운틴뷰, 구룡포 해수욕장, 포항 구룡포 과메기문화관, 구룡포항, 구룡포시장",
   icons: {
     icon: '/grami_icon.png',
   },
+  keywords: [
+    '포항',
+    '포항호텔',
+    '포항숙소',
+    '그라미호텔',
+    '포항 남구호텔',
+    '풀빌라',
+    '테라스',
+    '오션뷰',
+    '마운틴뷰',
+    '구룡포 해수욕장',
+    '포항 구룡포 과메기문화관',
+    '구룡포항',
+    '구룡포시장',
+    '일출'
+  ],
   verification: {
     google: 'xXx3IWwiMA0Fx9tekBLB2C9PNdZTu9f1w1bU_ygV6nU',
     other: {
       'naver-site-verification': '00810c0a9432a58a26e3dc58db486ce13634b299',
     },
+  },
+  openGraph: {
+    type: 'website',
+    title: '그라미호텔',
+    description:
+      '포항, 포항호텔, 포항숙소, 그라미호텔, 포항 남구호텔, 풀빌라, 테라스, 오션뷰, 마운틴뷰, 구룡포 해수욕장, 구룡포시장, 일출',
+    url: 'https://www.gramihotel.co.kr',
+    images: [
+      {
+        url: 'https://storage.googleapis.com/grammy_static/e608212b-89b7-4fd1-bc10-a97766ac0cb6.jpg',
+      },
+    ],
+  },
+  alternates: {
+    canonical: 'https://www.gramihotel.co.kr',
   },
 };
 
